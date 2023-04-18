112aadbd828bee2f39f7949e12f1501871444c2e
J-lng <jlungu.higgins@gmail.com>
2023-04-18 16:33:11 +0100

    trades model

--- trades/models.py
+++ trades/models.py
@@ -4,11 +4,13 @@ from django.db import models
 from prices.models import Price
 from assets.models import Asset
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 
 class Trade(models.Model):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     execution_timestamp = models.DateTimeField()
     volume = models.FloatField()
     BUY = 'BUY'
