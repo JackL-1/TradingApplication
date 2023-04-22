ca95f63203fdd17db78e329345cdce06beb17a80
J-lng <jlungu.higgins@gmail.com>
2023-04-22 21:47:15 +0100

    staging

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -23,6 +23,17 @@ const Home = () => {
     { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: 5.372 },
     { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Sell', pnl: 2.86 },
     { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: -3.874 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: -3.874 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: 5.372 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Sell', pnl: 2.86 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: -3.874 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: 5.372 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Sell', pnl: 2.86 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: -3.874 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: 5.372 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Sell', pnl: 2.86 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: -3.874 },
+    { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: 5.372 },
     { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: 5.372 },
     { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Sell', pnl: 2.86 },
     { Ticker: 'AAPL', Quantity: '5.00', ExecutionPrice: '@166.75', ExecutionDate: '2023-04-21 13:10:43.78', buysell: 'Buy', pnl: -3.874 },
@@ -32,7 +43,7 @@ const Home = () => {
   return (
     <>
       <section className='background'>
-        <div className="TradeTable" style={{ height: '100px' }}> 
+        <div className="tradetabledefaultpage" > 
           <Tradetable data={data} />
         </div>
       </section>
