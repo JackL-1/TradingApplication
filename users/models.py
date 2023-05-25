fd453a058435407e0e2d8571a406bef6c0fe2477
J-lng <jlungu.higgins@gmail.com>
2023-05-25 09:23:47 +0100

    minlength changed from 5 to 3

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinLengthValidator
 from django.core.validators import EmailValidator
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20,unique=True, validators=[MinLengthValidator(5)])
+    username = models.CharField(max_length=20,unique=True, validators=[MinLengthValidator(3)])
     email = models.CharField(max_length=50, unique=True, validators=[EmailValidator()])
     funds = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
